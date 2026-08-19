7b064be378a5f55951492deb460f087986bfb1ea
SAMUVEL2007 <ssamuvel890@gmail.com>
2026-08-19 19:01:51 +0530

    Add files via upload

---
+++ Lab 3 - Task 3.py
@@ -0,0 +1,86 @@
+#For loop
+
+
+#Question 1:
+
+for i in range (1,11):
+    print(i)
+
+
+#Question 2:
+
+for i in range (10,0,-1):
+    print(i)
+
+
+#Question 3:
+
+for i in range (1,21):
+    if i%2==0:
+        print(i)
+
+
+#Question 4:
+
+for i in range (1,21):
+    if i%2!=0:
+        print(i)
+
+
+#Question 5:
+
+for i in range (1,11):
+        print(' 5 ','* ',i,' = ',5*i)
+
+
+#Question 6:
+
+s=0
+for i in range (1,101):
+    s=s+i
+print('sum of 1 to 100 is ',s)
+    
+
+
+#Question 7:
+
+s=0
+for i in range (1,51):
+    if i%2==0:
+        s=s+i
+print('sum of even 1 to 50  is ',s)
+
+
+
+#Question 8:
+
+a='PYTHON'
+for i in a:
+    print(i)
+
+
+#Question 9:
+
+a='SAMUVEL'
+s=0
+for i in a:
+    s=s+1
+print(s)
+
+
+
+#Question 10:
+
+for i in range(1,11):
+    print(i,'** 2  = ',i**2)
+
+
+
+
+
+
+
+
+
+
+
